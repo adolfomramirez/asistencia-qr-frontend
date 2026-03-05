9ba47ca61f89f729812ff3e69013a101fd7967f5
adolfomramirez <202105870@udv.edu.gt>
2026-03-05 00:23:05 -0600

    Agrego componente básico Asistencia

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
       <Text style={{ fontSize: 24, color: '#333' }}>
-        App QR ya está funcionando
+        App QR Adolfo
       </Text>
     </View>
   );
